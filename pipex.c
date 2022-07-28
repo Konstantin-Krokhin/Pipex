b22ae8deb4c26f2733a50198fac79895687191b7
Konstantin-Krokhin <winpie1@gmail.com>
2022-07-28 18:00:42 +0200

    The command worksgit add . Such as ./a.out filename cmd, left to do is the pipe functionality using fork and processes

--- pipex.c
+++ pipex.c
@@ -4,33 +4,23 @@
 #include "fcntl.h" 						// ONLY FOR TESTING !
 #include "TESTONLY_GNL/get_next_line.h" // ONLY FOR TESTING !
 
-int main (int argc, char **argv, char **envp)
+char *getting_cmd_path(char **argv, char **envp)
 {
-//char *options[3] = {"ls", "-l", NULL};
-//execve("/bin/ls", options, envp);
-
-
-	// FOR Printing with GNL
-	//int fd = open("/Users/konsta/Desktop/42/Pipex/.vscode/environment_variables.txt", O_RDONLY);
-	//int i = -1;
-	//while (envp[++i] != NULL)
-	//	envp[i] = get_next_line(fd);
-
+	// --- PARSING ENVIRONMENTAL VARIABLES
 	char *env_path = NULL;
 	char **paths;
-	char *cmd = "ls";
 	int i = -1;
 
 	while (envp[++i])
 		if (ft_strnstr(envp[i], "PATH=", ft_strlen(envp[i])) != NULL)
 		{
 			env_path = ft_substr(envp[i], 5, ft_strlen(envp[i]));
-			printf("	PATHS: %s\n", env_path);
 			break;
 		}
 	// retrieve **path variable envp and divide it with split by ':'
 	paths = ft_split(env_path, ':');
 
+	// -------- ADDING / to end of each path to check them later
 	i = -1;
 	char *tmp;
 	// add '/' sign with strjoin to each path element
@@ -41,17 +31,34 @@ int main (int argc, char **argv, char **envp)
 		free(tmp);
 	}
 	
+	// -------- CHECKING ACCESS ---------
+
 	i = -1;
 	char *cmd_path;
 	while (paths[++i])
 	{
-		cmd_path = ft_strjoin(paths[i], cmd);
+		cmd_path = ft_strjoin(paths[i], argv[2]);
 		if (!(access(cmd_path, F_OK | X_OK)))
 			break;
 		free(cmd_path);
 	}
+	return(cmd_path);
+}
+
+int main (int argc, char **argv, char **envp)
+{
+//char *options[3] = {"ls", "-l", NULL};
+//execve("/bin/ls", options, envp);
+
+	char *cmd_path;
 	
-	printf("	cmdpath = %s\n", cmd_path);
+	if (!(cmd_path = getting_cmd_path(argv, envp)))
+	{
+		perror(cmd_path);
+		return (-1);
+	}
+	execve(cmd_path, argv, envp);
+	//printf("	cmdpath = %s\n", cmd_path);
 
 	//execve("/bin/ls", options, envp);
 
